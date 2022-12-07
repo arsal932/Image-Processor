18e3432c911db4ffe50f429cf62aa5bd06da3c60
Shah Fahad Ahmad <sfahad566@gmail.com>
2022-12-08 01:06:58 +0500

    - Text Customization features added.

--- Image_Processor/Image_Processor/wwwroot/js/Canvas_Editor.js
+++ Image_Processor/Image_Processor/wwwroot/js/Canvas_Editor.js
@@ -34,7 +34,9 @@ objects.push({
     Text: 'This is text area',
     fontColor: 'black',
     fontSize: 12,
-    fontFamily: 'Georgia'
+    fontFamily: 'Georgia',
+    isBold: false,
+    isItalic: false
 });
 objects.push({
     type: 'text',
@@ -45,8 +47,10 @@ objects.push({
     color: 'blue',
     Text: 'This is text area\nThis is text areaThis is text area',
     fontColor: 'white',
-    fontSize: 20,
-    fontFamily: 'Georgia'
+    fontSize: 12,
+    fontFamily: 'Georgia',
+    isBold: false,
+    isItalic: false
 });
 objects.push({
     type: 'img',
@@ -88,8 +92,10 @@ let add_label_object = function() {
         color: 'black',
         Text: 'This is your line 1\n This is your line2. This is your line2 \n This is your line3',
         fontColor: 'white',
-        fontSize: 20,
-        fontFamily: 'Georgia'
+        fontSize: 12,
+        fontFamily: 'Georgia',
+        isBold: false,
+        isItalic: false
     });
     draw_objects();
 }
@@ -113,29 +119,173 @@ let is_mouse_in_object = function(x, y, object) {
     }
     return false;
 }
-let enable_elements = function(id) {
-    document.getElementById(id).disabled = false;
+let fill_text = function() {
+    if (current_object_index != null) {
+        document.getElementById("label_text").value = objects[current_object_index].Text;
+    }
 }
-let disable_elements = function(id) {
-    document.getElementById(id).disabled = true;
+let active_links = function(id) {
+    document.getElementById(id).classList.add("active");
+}
+let show_elements = function(id) {
+    document.getElementById(id).style.display = "block";
+}
+let hide_elements = function(id) {
+    document.getElementById(id).style.display = "none";
+};
+let show_elements_for_label = function() {
+    show_elements("text_el_b");
+    show_elements("text_el_i");
+    show_elements("text_el_t");
+    show_elements("text_el_u");
+    show_elements("text_el_l");
+    show_elements("text_el_fs");
+    show_elements("text_el_ff");
+    show_elements("text_el_fc");
+}
+let hide_elements_of_label = function() {
+    hide_elements("text_el_b");
+    hide_elements("text_el_i");
+    hide_elements("text_el_t");
+    hide_elements("text_el_u");
+    hide_elements("text_el_l");
+    hide_elements("text_el_fs");
+    hide_elements("text_el_ff");
+    hide_elements("text_el_fc");
+}
+let set_font_size = function() {
+    document.getElementById("size_text").value = objects[current_object_index].fontSize;
+}
+let change_font_size = function() {
+    if (current_object_index != null) {
+        var object = objects[current_object_index];
+        object.fontSize = parseInt(document.getElementById("size_text").value);
+        objects[current_object_index] = object;
+        draw_objects();
+    }
+}
+let set_font_color = function() {
+    document.getElementById("text_color").value = objects[current_object_index].fontColor;
+}
+let change_font_color = function() {
+    var color = document.getElementById("text_color").value;
+    if (current_object_index != null) {
+        var object = objects[current_object_index];
+        object.fontColor = color;
+        objects[current_object_index] = object;
+        draw_objects();
+    }
+}
+
+function change_object_color(id) {
+
+    var color = document.getElementById(id).value;
+    if (current_object_index != null) {
+        var object = objects[current_object_index];
+        object.color = color;
+        objects[current_object_index] = object;
+        draw_objects();
+    }
+}
+
+function bold_text() {
+    if (current_object_index != null) {
+        var object = objects[current_object_index];
+        if (object.isBold) {
+            object.isBold = false;
+        } else {
+            object.isBold = true;
+        }
+        objects[current_object_index] = object;
+        draw_objects();
+    }
+}
+
+function italic_text() {
+    if (current_object_index != null) {
+        var object = objects[current_object_index];
+        if (object.isItalic) {
+            object.isItalic = false;
+        } else {
+            object.isItalic = true;
+        }
+        objects[current_object_index] = object;
+        draw_objects();
+    }
+}
+
+function tolower_text() {
+    if (current_object_index != null) {
+        var object = objects[current_object_index];
+        object.Text = object.Text.toLowerCase();
+        objects[current_object_index] = object;
+        draw_objects();
+    }
+}
+
+function toupper_text() {
+    if (current_object_index != null) {
+        var object = objects[current_object_index];
+        object.Text = object.Text.toUpperCase();
+        objects[current_object_index] = object;
+        draw_objects();
+    }
+}
+
+function change_label_text() {
+    var Text = document.getElementById("label_text").value;
+    if (current_object_index != null) {
+        var object = objects[current_object_index];
+        object.Text = Text;
+        objects[current_object_index] = object;
+        draw_objects();
+    }
 }
 let mouse_down = function(event) {
     event.preventDefault();
-    disable_elements("_color");
+    hide_elements("background_color");
+    hide_elements_of_label();
     startX = parseInt(event.clientX - offset_x);
     startY = parseInt(event.clientY - offset_Y);
     let index = 0;
+    let isFound = false;
     for (let object of objects) {
         if (is_mouse_in_object(startX, startY, object)) {
             current_object_index = index;
             is_dragging = true;
+            isFound = true;
             if (object.type != "img") {
-                enable_elements("_color");
+                show_elements("background_color");
             }
+            if (object.type == "text") {
+                show_elements_for_label();
+                fill_text();
+                set_font_size();
+                set_font_color();
+                if (object.isBold) {
+                    active_links("bold_text");
+                }
+                if (object.isItalic) {
+                    active_links("italic_text");
+                }
+            }
+            //context.fillStyle = "black";
+            //context.fillRect(object.x + 2, object.y + 2, object.width + 2, object.height + 2);
             //return;
         }
         index++;
     }
+    let currentObject = objects[current_object_index];
+    //let lastObject = objects[objects.length - 1];
+    //objects[current_object_index] = lastObject;
+    //objects[objects.length - 1] = currentObject;
+    //current_object_index = objects.length - 1;
+    //if (isFound) {
+    //  objects.splice(current_object_index, current_object_index);
+    //objects.push(currentObject);
+    //current_object_index = objects.length - 1;
+    //}
+    isFound = false;
 }
 
 let mouse_up = function(event) {
@@ -165,14 +315,14 @@ let mouse_move = function(event) {
         let dx = mouseX - startX;
         let dy = mouseY - startY;
 
-        for (let object of objects) {
-            let current_object = objects[current_object_index];
-            current_object.x += dx;
-            current_object.y += dy;
-            draw_objects();
-            startX = mouseX;
-            startY = mouseY;
-        }
+        //for (let object of objects) {
+        let current_object = objects[current_object_index];
+        current_object.x += dx;
+        current_object.y += dy;
+        draw_objects();
+        startX = mouseX;
+        startY = mouseY;
+        //}
     }
 }
 
@@ -182,9 +332,9 @@ canvas.onmouseout = mouse_out
 canvas.onmousemove = mouse_move;
 
 let draw_objects = function() {
-
     context.clearRect(0, 0, canvas_width, canvas_height);
     for (let object of objects) {
+        //context.globalAlpha = 0.4;
         if (object.type == "rect") {
             context.fillStyle = object.color;
             context.fillRect(object.x, object.y, object.width, object.height);
@@ -200,8 +350,18 @@ let draw_objects = function() {
             object.height = space + (lines.length * object.fontSize);
             context.fillRect(object.x, object.y, object.width, object.height);
             for (var i = 0; i < lines.length; i++) {
+
+                let fillstr = "";
+                if (object.isItalic) {
+                    fillstr += "italic ";
+                }
+                if (object.isBold) {
+                    fillstr += "bold ";
+                }
+                fillstr += object.fontSize + "px" + " " + object.fontFamily;
+                console.log(fillstr);
                 context.fillStyle = object.fontColor;
-                context.font = "12px Georgia";
+                context.font = fillstr;
                 context.fillText(lines[i], object.x + padding, object.y + ((i + 1) * object.fontSize), object.width);
             }
         } else if (object.type == "img") {
