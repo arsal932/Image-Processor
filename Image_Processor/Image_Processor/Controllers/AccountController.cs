66cff9eaefa81ccc7b37bbf6c02a5f644e1e5488
Shah Fahad Ahmad <sfahad566@gmail.com>
2022-12-14 23:10:06 +0500

    - UI Updated - Login,Signup,Forgot passowrd UI added.

---
+++ Image_Processor/Image_Processor/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Image_Processor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Image_Processor.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Authentication()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            return RedirectToAction("Authentication");
+        }
+        [HttpGet]
+        public async Task<IActionResult> SignUp()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel signUpViewModel)
+        {
+            return RedirectToAction("Authentication");
+        }
+        [HttpGet]
+        public async Task<IActionResult> ForgotPassword()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel forgotPasswordViewModel)
+        {
+            return RedirectToAction("Authentication");
+        }
+    }
+}
---
+++ Image_Processor/Image_Processor/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Image_Processor.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
