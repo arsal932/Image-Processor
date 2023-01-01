3b6bf5b56edf6db6bf63b7a88fca37ca47469ebb
Shah Fahad Ahmad <sfahad566@gmail.com>
2023-01-01 23:58:32 +0500

    - Audio Track added to record video.

--- Image_Processor/Image_Processor/wwwroot/js/Canvas_Editor.js
+++ Image_Processor/Image_Processor/wwwroot/js/Canvas_Editor.js
@@ -8,15 +8,15 @@ let canvas_height = canvas.height;
 let offset_x;
 let offset_Y;
 
-let get_Offset = function() {
+let get_Offset = function () {
     let canvas_offsets = canvas.getBoundingClientRect();
     offset_x = canvas_offsets.left;
     offset_Y = canvas_offsets.top;
 }
 get_Offset();
-window.onscroll = function() { get_Offset(); }
-window.onresize = function() { get_Offset(); }
-canvas.onresize = function() { get_Offset(); }
+window.onscroll = function () { get_Offset(); }
+window.onresize = function () { get_Offset(); }
+canvas.onresize = function () { get_Offset(); }
 
 let pages = [];
 let currentPage = 0;
@@ -79,7 +79,7 @@ function createPoster(path, isStart, value) {
     canvas_1.getContext("2d").drawImage(video, 0, 0, canvas_1.width, canvas_1.height);
     return canvas_1.toDataURL("image/jpeg");
 }
-let add_image_object = function(path) {
+let add_image_object = function (path) {
     var image = new Image();
     image.src = path;
     objects.push({
@@ -102,7 +102,7 @@ let add_image_object = function(path) {
     });
     draw_objects();
 }
-let add_Gif_object = function(path) {
+let add_Gif_object = function (path) {
     var image = new Image();
     image.src = path;
     objects.push({
@@ -118,7 +118,7 @@ let add_Gif_object = function(path) {
     });
     draw_objects();
 }
-let add_Video_object = function(path) {
+let add_Video_object = function (path) {
     // const video = document.createElement('video');
     //video.src = path + "#t=0";
     //video.autoplay = false;
@@ -138,7 +138,7 @@ let add_Video_object = function(path) {
     });
     draw_objects();
 }
-let add_rectangle_shape_object = function(shape) {
+let add_rectangle_shape_object = function (shape) {
     objects.push({
         type: shape,
         x: 0,
@@ -149,7 +149,7 @@ let add_rectangle_shape_object = function(shape) {
     });
     draw_objects();
 }
-let add_square_shape_object = function(shape) {
+let add_square_shape_object = function (shape) {
     objects.push({
         type: shape,
         x: 0,
@@ -160,7 +160,7 @@ let add_square_shape_object = function(shape) {
     });
     draw_objects();
 }
-let add_circle_shape_object = function(shape) {
+let add_circle_shape_object = function (shape) {
     objects.push({
         type: shape,
         x: 50,
@@ -172,7 +172,7 @@ let add_circle_shape_object = function(shape) {
     });
     draw_objects();
 }
-let add_oval_shape_object = function(shape) {
+let add_oval_shape_object = function (shape) {
     objects.push({
         type: shape,
         x: 50,
@@ -184,7 +184,7 @@ let add_oval_shape_object = function(shape) {
     });
     draw_objects();
 }
-let add_label_object = function() {
+let add_label_object = function () {
     objects.push({
         type: 'text',
         x: 0,
@@ -201,17 +201,17 @@ let add_label_object = function() {
     });
     draw_objects();
 }
-let open_text_panel = function() {
+let open_text_panel = function () {
     document.getElementById("text_panel").style.display = "block";
 }
-let hide_text_panel = function() {
+let hide_text_panel = function () {
     document.getElementById("text_panel").style.display = "none";
 }
-let default_form_open = function() {
+let default_form_open = function () {
     document.getElementById("templates_panel").style.display = "block";
 }
 default_form_open();
-let is_mouse_in_object = function(x, y, object) {
+let is_mouse_in_object = function (x, y, object) {
     if (object.type == "circle" || object.type == "oval") {
         var areaX = x - object.x;
         var areaY = y - object.y;
@@ -228,44 +228,47 @@ let is_mouse_in_object = function(x, y, object) {
         return false;
     }
 }
-let set_font_family = function() {
+let set_font_family = function () {
     if (current_object_index != null) {
         $('#fontfamily_text').val(objects[current_object_index].fontFamily);
     }
 }
 
-let fill_text = function() {
+let fill_text = function () {
     if (current_object_index != null) {
         document.getElementById("label_text").value = objects[current_object_index].Text;
     }
 }
-let active_links = function(id) {
+let active_links = function (id) {
     document.getElementById(id).classList.add("active");
 }
-let show_elements = function(id) {
+let show_elements = function (id) {
     document.getElementById(id).style.display = "block";
 }
-let hide_elements = function(id) {
-    document.getElementById(id).style.display = "none";
+let hide_elements = function (id) {
+    var elmnt = document.getElementById(id);
+    if (elmnt != null) {
+        elmnt.style.display = "none";
+    }
 };
-let show_elements_for_label = function() {
+let show_elements_for_label = function () {
     show_elements("text_els");
     show_elements("text_el_t");
     show_elements("text_el_fs");
     show_elements("text_el_ff");
     show_elements("text_el_fc");
 }
-let hide_elements_of_label = function() {
+let hide_elements_of_label = function () {
     hide_elements("text_els");
     hide_elements("text_el_t");
     hide_elements("text_el_fs");
     hide_elements("text_el_ff");
     hide_elements("text_el_fc");
 }
-let set_font_size = function() {
+let set_font_size = function () {
     document.getElementById("size_text").value = objects[current_object_index].fontSize;
 }
-let change_font_size = function() {
+let change_font_size = function () {
     if (current_object_index != null) {
         var object = objects[current_object_index];
         object.fontSize = parseInt(document.getElementById("size_text").value);
@@ -273,10 +276,10 @@ let change_font_size = function() {
         draw_objects();
     }
 }
-let set_font_color = function() {
+let set_font_color = function () {
     document.getElementById("text_color").value = objects[current_object_index].fontColor;
 }
-let change_font_color = function() {
+let change_font_color = function () {
     var color = document.getElementById("text_color").value;
     if (current_object_index != null) {
         var object = objects[current_object_index];
@@ -350,7 +353,8 @@ function change_label_text() {
         draw_objects();
     }
 }
-let mouse_down = function(event) {
+let mouse_down = function (event) {
+
     event.preventDefault();
     hide_elements("background_color");
     hide_elements("img_els");
@@ -361,6 +365,7 @@ let mouse_down = function(event) {
     let isFound = false;
     for (let object of objects) {
         if (is_mouse_in_object(startX, startY, object)) {
+            console.log("Im in object");
             current_object_index = index;
             is_dragging = true;
             isFound = true;
@@ -392,8 +397,11 @@ let mouse_down = function(event) {
         }
         index++;
     }
-    context.fillStyle = 'rgba(225,225,225,0.5)';
-    context.fillRect(objects[current_object_index].x, objects[current_object_index].y, objects[current_object_index].width, objects[current_object_index].height);
+    console.log(current_object_index);
+    if (current_object_index != null) {
+        context.fillStyle = 'rgba(225,225,225,0.5)';
+        context.fillRect(objects[current_object_index].x, objects[current_object_index].y, objects[current_object_index].width, objects[current_object_index].height);
+    }
     //let currentObject = objects[current_object_index];
     //let lastObject = objects[objects.length - 1];
     //objects[current_object_index] = lastObject;
@@ -408,7 +416,7 @@ let mouse_down = function(event) {
 }
 
 
-let mousedbl_click = function(event) {
+let mousedbl_click = function (event) {
     event.preventDefault();
     hide_elements("background_color");
     hide_elements_of_label();
@@ -443,14 +451,14 @@ let mousedbl_click = function(event) {
         });
     }
 }
-let mouse_up = function(event) {
+let mouse_up = function (event) {
     if (!is_dragging) {
         return;
     }
     event.preventDefault();
     is_dragging = false;
 }
-let mouse_out = function(event) {
+let mouse_out = function (event) {
     if (!is_dragging) {
         return;
     }
@@ -458,7 +466,7 @@ let mouse_out = function(event) {
     is_dragging = false;
 }
 
-let mouse_move = function(event) {
+let mouse_move = function (event) {
 
     if (!is_dragging) {
         return;
@@ -488,11 +496,54 @@ canvas.onmousemove = mouse_move;
 let recording = false;
 let mediaRecorder;
 let recordedChunks;
-let record = function() {
+let record = function () {
+    //console.log("start");
+    //const stream = canvas.captureStream(25);
+    //mediaRecorder = new MediaRecorder(stream, {
+    //    mimeType: 'video/webm;codecs=vp9',
+    //    ignoreMutedMedia: true
+    //});
+    //recordedChunks = [];
+    //mediaRecorder.ondataavailable = e => {
+    //    if (e.data.size > 0) {
+    //        recordedChunks.push(e.data);
+    //    }
+    //};
+    //mediaRecorder.start();
+    //console.log("before");
+    ////recordBtn.textContent = "Record"
+    //console.log(mediaRecorder);
+    //setTimeout(() => {
+    //    console.log(mediaRecorder);
+    //    mediaRecorder.stop();
+    //    const blob = new Blob(recordedChunks, {
+    //        type: "video/webm"
+    //    });
+    //    const url = URL.createObjectURL(blob);
+    //    const a = document.createElement("a");
+    //    a.href = url;
+    //    a.download = "recording.webm";
+    //    a.click();
+    //    URL.revokeObjectURL(url);
+    //}, 5000);
+
+
     recording = !recording;
     if (recording) {
         //recordBtn.textContent = "Stop";
-        const stream = canvas.captureStream(25);
+        var stream = canvas.captureStream(25);
+        var audio = document.getElementById("audio-player");        
+        audio.play();
+
+        // get the audio track:
+        let ctx = new AudioContext();
+        let dest = ctx.createMediaStreamDestination();
+        let sourceNode = ctx.createMediaElementSource(audio);
+        sourceNode.connect(dest);
+        sourceNode.connect(ctx.destination);
+        let audioTrack = dest.stream.getAudioTracks()[0];
+
+        stream.addTrack(audioTrack);
         mediaRecorder = new MediaRecorder(stream, {
             mimeType: 'video/webm;codecs=vp9',
             ignoreMutedMedia: true
@@ -505,7 +556,7 @@ let record = function() {
         };
         mediaRecorder.start();
     } else {
-        //recordBtn.textContent = "Record"
+        //recordBtn.textContent = "Record"                  
         mediaRecorder.stop();
         setTimeout(() => {
             const blob = new Blob(recordedChunks, {
@@ -520,7 +571,7 @@ let record = function() {
         }, 0);
     }
 }
-let draw_objects = function() {
+let draw_objects = function () {
     context.clearRect(0, 0, canvas_width, canvas_height);
     load_objects();
     for (let object of objects) {
@@ -573,7 +624,7 @@ let obj = null;
 function onDrawFrame(ctx, frame) {
     ctx.drawImage(frame.buffer, obj.x, obj.y, obj.width, obj.height);
 }
-let update_gifs = function() {
+let update_gifs = function () {
     var index = 0;
     for (let object of objects) {
         if (object.type == "img") {
@@ -595,7 +646,7 @@ let update_gifs = function() {
     }
     update_page();
 }
-let update_page = function() {
+let update_page = function () {
     if (currentPage >= 0) {
         //var img = new Image();
         //img.src = document.getElementById('canvas').toDataURL();
@@ -605,7 +656,7 @@ let update_page = function() {
         document.getElementById(crntPage).src = document.getElementById('canvas').toDataURL();
     }
 }
-let load_objects = function() {
+let load_objects = function () {
     if (pages.length < 1) {
         emptyObjects = [];
         pages.push(emptyObjects);
@@ -615,7 +666,7 @@ let load_objects = function() {
     }
     objects = pages[currentPage];
 }
-let find_max_width = function(lines) {
+let find_max_width = function (lines) {
     let max_width = 0;
     for (var i = 0; i < lines.length; i++) {
         if (context.measureText(lines[i]).width > max_width) {
