b5d2be6aa1964516610a0a17523f843667ca5b28
Shah Fahad Ahmad <sfahad566@gmail.com>
2023-01-10 01:10:28 +0500

    - UI Updated. - Projects UI Added.

---
+++ Image_Processor/Image_Processor/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Image_Processor.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult view()
+        {
+            return View();
+        }
+    }
+}
