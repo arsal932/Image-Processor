2da502aefd1b2b7c3212d2f0dc89bdfc5fb7fe30
Shah Fahad Ahmad <sfahad566@gmail.com>
2023-01-12 00:06:47 +0500

    - UI Updated. - Home page updated. - project page completed.

--- Image_Processor/Image_Processor/Controllers/ProjectsController.cs
+++ Image_Processor/Image_Processor/Controllers/ProjectsController.cs
@@ -8,5 +8,30 @@ namespace Image_Processor.Controllers
         {
             return View();
         }
+        public IActionResult GetList()
+        {
+            Thread.Sleep(2000);
+            return PartialView();
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult Edit(string name)
+        {
+            return RedirectToAction("view");
+        }
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            return RedirectToAction("view");
+        }
     }
 }
